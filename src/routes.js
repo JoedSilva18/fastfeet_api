51c74c07eeeb0d45e90098e9a5330fb72a581cad
Joed Silva <joedsilva2016@gmail.com>
2020-02-02 20:44:01 -0300

    Feature - Suporte a envio de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import FileController from './app/controllers/FileController';
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.post('/recipient', RecipientController.store);
 routes.put('/recipient/:id', RecipientController.update);
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
